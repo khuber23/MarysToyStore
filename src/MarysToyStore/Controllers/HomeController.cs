df00cc45ec6333fd264dd27948d1b7f1d39ae2bb
Kayla <kehuber@students.ntc.edu>
2023-01-30 00:18:57 -0600

    1MaryToyStore

---
+++ src/MarysToyStore/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarysToyStore.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return new ContentResult { Content = "Hello world!" };
+    }
+}
