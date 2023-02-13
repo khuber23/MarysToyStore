590ec64950c41ed10093f68dba80ff3f96ebf96e
Kayla <kehuber@students.ntc.edu>
2023-02-12 19:05:56 -0600

    3MarysToyStore

--- src/MarysToyStore/Controllers/HomeController.cs
+++ src/MarysToyStore/Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 ﻿using MarysToyStore.Models;
+using MarysToyStore.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
 namespace MarysToyStore.Controllers;
 
+[Route("")]
 public class HomeController : Controller
 {
     private readonly AppConfig _appConfig;
 
     private readonly ILogger<HomeController> _logger;
 
+    private readonly DataService _dataService;
+
     public HomeController(ILogger<HomeController> logger, IOptions<AppConfig> appConfigWrapper)
     {
         _logger = logger;
         _appConfig = appConfigWrapper.Value;
+        _dataService = new DataService();
     }
 
+    [Route("")]
     public IActionResult Index()
     {
         // Add data to the viewbag (making it accessible to the returned view).
@@ -24,21 +30,31 @@ public class HomeController : Controller
         return View();
     }
 
+    [Route("about")]
     public IActionResult About()
     {
         return View();
     }
 
-    public IActionResult Product()
+    [Route("product/{id:int}")]
+    public IActionResult Product(int id)
     {
-        Product model = new()
-        {
-        Id = 5,
-        Name = "Electric scooter",
-        Description = "A cool, stylish scooter that won't start on fire!",
-        Price = 18.99M
-        };
+        Product model = _dataService.GetProduct(id);
 
         return View(model);
+    }
+
+    [Route("products")]
+    public IActionResult Products()
+    {
+        List<Product> model = _dataService.GetProducts();
+
+        return View(model);
+    }
+
+    [Route("error")]
+    public IActionResult Error()
+    {
+        return View();
     }
 }
---
+++ src/MarysToyStore/Services/DataService.cs
@@ -0,0 +1,29 @@
+﻿namespace MarysToyStore.Services
+{
+    public class DataService
+    {
+        private List<Product> products;
+
+        public DataService()
+        {
+            products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Car", Description = "A toy car that goes really fast.", Price = 3.99m, ImagePath = "/bluecar.jpg" },
+                new Product { Id = 2, Name = "Ducks", Description = "Toy ducks that float.", Price = 10.99m, ImagePath = "/ducks.jpg" },
+                new Product { Id = 3, Name = "Legos", Description = "A toy to build your ideas.", Price = 25.99m, ImagePath = "/legos.jpg" },
+                new Product { Id = 4, Name = "Robot", Description = "An advanced toy that will make anybody happy.", Price = 15.99m, ImagePath = "/robot.jpg" },
+                new Product { Id = 5, Name = "Teddy", Description = "A soft bear that is comforting to touch.", Price = 29.99m, ImagePath = "/teddy.jpg" }
+            };
+        }
+
+        public List<Product> GetProducts()
+        {
+            return products;
+        }
+
+        public Product GetProduct(int id)
+        {
+           return products.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
