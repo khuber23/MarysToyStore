b5f555990d3dc0429685036e54860cd504192632
Kayla <kehuber@students.ntc.edu>
2023-02-17 19:22:33 -0600

    4MarysToyStore

--- src/MarysToyStore/Data/DataContext.cs
+++ src/MarysToyStore/Data/DataContext.cs
@@ -8,5 +8,19 @@ public class DataContext : DbContext
         : base(options)
     { }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Specifiy the seed data.
+        modelBuilder.Entity<Product>().HasData(
+                    new Product { Id = 1, Name = "Car", Description = "A toy car that goes really fast.", Price = 3.99m, ImagePath = "/bluecar.jpg" },
+                    new Product { Id = 2, Name = "Ducks", Description = "Toy ducks that float.", Price = 10.99m, ImagePath = "/ducks.jpg" },
+                    new Product { Id = 3, Name = "Legos", Description = "A toy to build your ideas.", Price = 25.99m, ImagePath = "/legos.jpg" },
+                    new Product { Id = 4, Name = "Robot", Description = "An advanced toy that will make anybody happy.", Price = 15.99m, ImagePath = "/robot.jpg" },
+                    new Product { Id = 5, Name = "Teddy", Description = "A soft bear that is comforting to touch.", Price = 29.99m, ImagePath = "/teddy.jpg" }
+        );
+    }
+
     public DbSet<Product> Products { get; set; }
 }
--- src/MarysToyStore/Services/DataService.cs
+++ src/MarysToyStore/Services/DataService.cs
@@ -1,29 +1,25 @@
-﻿namespace MarysToyStore.Services
+﻿using System.Linq;
+using MarysToyStore.Data;
+
+namespace MarysToyStore.Services
 {
     public class DataService
     {
-        private List<Product> products;
+        private readonly DataContext _dataContext;
 
-        public DataService()
+        public DataService(DataContext dataContext)
         {
-            products = new List<Product>
-            {
-                new Product { Id = 1, Name = "Car", Description = "A toy car that goes really fast.", Price = 3.99m, ImagePath = "/bluecar.jpg" },
-                new Product { Id = 2, Name = "Ducks", Description = "Toy ducks that float.", Price = 10.99m, ImagePath = "/ducks.jpg" },
-                new Product { Id = 3, Name = "Legos", Description = "A toy to build your ideas.", Price = 25.99m, ImagePath = "/legos.jpg" },
-                new Product { Id = 4, Name = "Robot", Description = "An advanced toy that will make anybody happy.", Price = 15.99m, ImagePath = "/robot.jpg" },
-                new Product { Id = 5, Name = "Teddy", Description = "A soft bear that is comforting to touch.", Price = 29.99m, ImagePath = "/teddy.jpg" }
-            };
+            _dataContext = dataContext;
         }
 
         public List<Product> GetProducts()
         {
-            return products;
+            return _dataContext.Products.ToList();
         }
 
         public Product GetProduct(int id)
         {
-           return products.FirstOrDefault(x => x.Id == id);
+            return _dataContext.Products.FirstOrDefault(x => x.Id == id);
         }
     }
 }
