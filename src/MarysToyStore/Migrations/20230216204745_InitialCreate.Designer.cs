1ed969e35b25c325677d456aa02ef9f576179853
Kayla <kehuber@students.ntc.edu>
2023-02-27 15:26:04 -0600

    Update migrations, they keep doubling

--- src/MarysToyStore/Migrations/20230216204745_InitialCreate.Designer.cs
+++ src/MarysToyStore/Migrations/20230216204745_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MarysToyStore.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20230216232550_InitialCreate")]
+    [Migration("20230216204745_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
